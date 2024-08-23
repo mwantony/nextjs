c930257abdd76d18f65ea3d498f231a804fa2616
Antony Maziero Warken <againplayi7@gmail.com>
2024-08-23 14:26:05 -0300

    asd

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -3,5 +3,15 @@ import styles from './Sobre.module.scss';
 export default function Sobre() {
   return <section className={styles['sobre']}>
     <p>sobre</p>
+    <button>aqui vem alguas</button>
+    <button>aqui vem alguas</button>
+    <button>aqui vem alguas</button>
+    <button>aqui vem alguas</button>
+    <button>aqui vem alguasSDSDD
+      
+    </button>
+    <div>alguma coias dsd
+      
+    </div>
   </section>
 }
